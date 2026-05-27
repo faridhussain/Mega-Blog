c3388dfeef065a6f094577f4d306fa134c4412e0
Farid Hussain <faridhussain0011@gmail.com>
2026-05-27 20:48:15 +0530

    setup app layout and authentication initialization

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,35 @@
+import { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux'
+import authService from './appwrite/auth.js'
+import { login, logout } from './store/authSlice.js'
+import { Footer, Header } from './components/index.js'
+import { Outlet } from 'react-router-dom'
+
 export default function App() {
-  return (
-    <h1>A blog app with appwrite</h1>
-  )
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if (userData) {
+        dispatch(login({ userData }))
+      } else {
+        dispatch(logout())
+      }
+    })
+    .finally(() => setLoading(false))
+  }, [])
+
+  return !loading ? (
+    <div className='min-h-screen'>
+      <div>
+        <Header />
+        <main>
+          <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null
 }
---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import Header from '../components/Header/Header.jsx'
+import Footer from '../components/Footer/Footer.jsx'
+
+export {
+    Header,
+    Footer
+}
